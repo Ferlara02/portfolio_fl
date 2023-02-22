13ef0148288ec15b6070ab333f8166905f8222ee
Fernando Lara <larafernandoagustin@gmail.com>
2023-02-22 02:06:50 -0300

    Componentes Header y Menu finalizados.

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Data = () => {
+    return (
+        <div className="home__data">
+            <h1 className="home__title">Fernando Lara</h1>
+            <h3 className="home__subtitle">Desarrollador Frontend</h3>
+            <p className="home__description">Hola! soy un Desarrollador web Frontend en continua formación, con el objetivo de convertirme en un Desarollador Fullstack.</p>
+
+            <a href="#contacto" className="button button--flex">Escríbeme! <i class="uil uil-navigator"></i></a>
+        </div>
+    )
+}
+
+export default Data
